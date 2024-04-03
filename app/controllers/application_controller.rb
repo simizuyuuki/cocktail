6a91d0fba93cc006392352a3a2c1fd90116151ff
simizuyuuki <riiman8713@gmail.com>
2024-04-03 14:20:56 +0900

    ユーザー認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from Exception, with: :error_handling
   rescue_from ActionController::RoutingError, with: :render_not_found
@@ -11,5 +12,10 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
