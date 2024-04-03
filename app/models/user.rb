eb2e4066c0b6d719b1cf63489c16104ca5e65ca1
simizuyuuki <riiman8713@gmail.com>
2024-04-03 18:29:21 +0900

    サインイン機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+         validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+         
          has_many :tweets
          has_one_attached :image
 end
