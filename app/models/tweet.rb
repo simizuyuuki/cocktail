6d40e32043df86678bd06264c2193507025a7166
simizuyuuki <riiman8713@gmail.com>
2024-04-08 21:22:01 +0900

    いいね機能仮実装

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :likes
   has_one_attached :image
   def self.search(search)
     if search != ""
@@ -9,6 +10,11 @@ class Tweet < ApplicationRecord
       Tweet.all
     end
   end
+
+  def liked_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
+  
   validates :name, :image,:text, presence: true 
   validates :text, length: { maximum: 168 }
 end
